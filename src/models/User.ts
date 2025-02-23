03647eac028ec769fb4a3b7d56c0a8da0bd09d91
shubham88fru <shubham88fru@yahoo.com>
2025-02-23 14:38:00 -0700

    extract attibute class.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
 
 export interface UserProps {
   name?: string;
@@ -6,17 +7,9 @@ export interface UserProps {
   id?: number;
 }
 
+const rootUrl = "http://localhost:3000/users";
+
 export class User {
   public events: Eventing = new Eventing();
-
-  constructor(private data: UserProps) {}
-
-  //Note: not a typical getter
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 }
