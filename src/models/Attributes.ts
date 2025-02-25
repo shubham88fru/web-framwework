7526cf8acbf41d9c76f2d853268f68a0d2918381
shubham88fru <shubham88fru@yahoo.com>
2025-02-25 14:59:08 -0700

    bunch of updates and fixes

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -2,11 +2,11 @@ export class Attributes<T extends object> {
   constructor(private data: T) {}
 
   //Note: not a typical getter
-  get(propName: string): string | number {
-    return this.data[propName];
+  get = <K extends keyof T>(key: K): T[K] => {
+    return this.data[key];
   }
 
-  set(update: T): void {
+  set = (update: T): void => {
     Object.assign(this.data, update);
   }
 }
