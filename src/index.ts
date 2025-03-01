bafc023f8c2bb5fd9d99a759aa68c6d2438326c6
shubham88fru <shubham88fru@yahoo.com>
2025-03-01 14:32:54 -0700

    pass view to model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import { User } from "./models/User";
 import { UserForm } from "./views/UserForm";
 
-const userForm = new UserForm(document.getElementById("root")!);
+const userForm = new UserForm(
+  document.getElementById("root")!,
+  User.buildUser({ name: "NAME", age: 20 })
+);
 
 userForm.render();
